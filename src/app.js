865f596f6546bc39be65857e1f945905079e54a7
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-04-26 18:45:28 +0000

    Primeros cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let num = Math.floor(Math.random() * 13) + 1;
+  if (num == 11) {
+    document.querySelector("h1").innerHTML = "J";
+  } else if (num == 12) {
+    document.querySelector("h1").innerHTML = "Q";
+  } else if (num == 13) {
+    document.querySelector("h1").innerHTML = "K";
+  } else {
+    document.querySelector("h1").innerHTML = num;
+  }
 };
