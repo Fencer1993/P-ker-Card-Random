4b5b3111377d7360fc5ad9bba5e27a7bf72a2465
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-04-27 07:32:20 +0000

    Últimos cambios, entregable

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-window.onload = function() {
+// Función general
+
+let generateRandomCard = () => {
+  // Los símbolos
+
   let symbols = ["♦", "♥", "♠", "♣"];
-  let numSymbol = Math.floor(Math.random() * 3);
+  let numSymbol = Math.floor(Math.random() * 4);
 
   document.querySelector(".upSymbol").innerHTML = symbols[numSymbol];
   if (symbols[numSymbol] == "♦") {
@@ -9,18 +13,25 @@ window.onload = function() {
   } else if (symbols[numSymbol] == "♥") {
     let heart = document.querySelector(".upSymbol");
     heart.style.color = "red";
+  } else {
+    let loDemas = document.querySelector(".upSymbol");
+    loDemas.style.color = "black";
   }
-  document.querySelector(".downSymbol").innerHTML = symbols[numSymbol];
 
+  document.querySelector(".downSymbol").innerHTML = symbols[numSymbol];
   if (symbols[numSymbol] == "♦") {
     let rombo = document.querySelector(".downSymbol");
     rombo.style.color = "red";
   } else if (symbols[numSymbol] == "♥") {
     let heart = document.querySelector(".downSymbol");
     heart.style.color = "red";
+  } else {
+    let loDemas = document.querySelector(".downSymbol");
+    loDemas.style.color = "black";
   }
 
-  let num = Math.floor(Math.random() * 13) + 1;
+  //  Números Aleatorios
+  let num = Math.floor(Math.random() * 13 + 1);
   if (num == 11) {
     document.querySelector("h1").innerHTML = "J";
   } else if (num == 12) {
@@ -35,45 +46,13 @@ window.onload = function() {
 
   if (symbols[numSymbol] == "♦" || symbols[numSymbol] == "♥") {
     document.querySelector("h1").style.color = "red";
+  } else {
+    document.querySelector("h1").style.color = "black";
   }
 };
-// Botón --------------------
-document.querySelector(".button").addEventListener("click", function() {
-  let symbols = ["♦", "♥", "♠", "♣"];
-  let numSymbol = Math.floor(Math.random() * 3);
 
-  document.querySelector(".upSymbol").innerHTML = symbols[numSymbol];
-  if (symbols[numSymbol] == "♦") {
-    let rombo = document.querySelector(".upSymbol");
-    rombo.style.color = "red";
-  } else if (symbols[numSymbol] == "♥") {
-    let heart = document.querySelector(".upSymbol");
-    heart.style.color = "red";
-  }
-  document.querySelector(".downSymbol").innerHTML = symbols[numSymbol];
+window.onload = generateRandomCard;
 
-  if (symbols[numSymbol] == "♦") {
-    let rombo = document.querySelector(".downSymbol");
-    rombo.style.color = "red";
-  } else if (symbols[numSymbol] == "♥") {
-    let heart = document.querySelector(".downSymbol");
-    heart.style.color = "red";
-  }
-
-  let num = Math.floor(Math.random() * 13) + 1;
-  if (num == 11) {
-    document.querySelector("h1").innerHTML = "J";
-  } else if (num == 12) {
-    document.querySelector("h1").innerHTML = "Q";
-  } else if (num == 13) {
-    document.querySelector("h1").innerHTML = "K";
-  } else if (num == 1) {
-    document.querySelector("h1").innerHTML = "A";
-  } else {
-    document.querySelector("h1").innerHTML = num;
-  }
-
-  if (symbols[numSymbol] == "♦" || symbols[numSymbol] == "♥") {
-    document.querySelector("h1").style.color = "red";
-  }
-});
+document
+  .querySelector("#generator")
+  .addEventListener("click", generateRandomCard);
